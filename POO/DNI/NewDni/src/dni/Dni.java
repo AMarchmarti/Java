20f5df8b7e796de72d32511226aa19d54b9ba273
AMarchmarti <marchmarti.antoni@gmail.com>
2019-01-16 19:31:53 +0100

    Add functions

--- POO/DNI/NewDni/src/dni/Dni.java
+++ POO/DNI/NewDni/src/dni/Dni.java
@@ -52,6 +52,33 @@ public class Dni {
         return this.dniSano;
     }
 
-    
+
+    public Boolean checkLength(){
+        if (getDni().length() == 9) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    public Boolean dniNumbers (String cadena){
+        try {
+            int num = Integer.parseInt(cadena.substring(0, cadena.length() - 1));
+            return true;
+        } catch (NumberFormatException num) {
+            return false;
+        }
+    }
+
+
+    public Boolean dniLetters (String cadena){
+        String newstring = cadena.substring(cadena.length() - 1);
+        if (table.indexOf(newstring) != -1){
+            return true;
+        }else{
+            return true;}
+    }
 
 }
