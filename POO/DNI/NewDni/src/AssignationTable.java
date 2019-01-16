d03b1e32ca97825b95c9988773ce7e4614b9975d
AMarchmarti <marchmarti.antoni@gmail.com>
2019-01-16 18:06:05 +0100

    Test cases pass

--- POO/DNI/NewDni/src/AssignationTable.java
+++ POO/DNI/NewDni/src/AssignationTable.java
@@ -29,9 +29,20 @@ public class AssignationTable {
         String Dni = "43180005";
         char letter = tabla.findLetter(Dni);
         char healthyletter = 'N';
-        if (letter == healthyletter){
+        if (letter == healthyletter) {
             System.out.println("Este DNI es correcto");
         }
-    }
 
+        String Dni2 = "43215351";
+        if (tabla.findLetter(Dni2) == 'F') {
+            System.out.println("Este DNI es correcto");
+        }
+
+        /* Incorrect Test Case */
+        if (tabla.findLetter(Dni2) == 'J') {
+            System.out.println("Este DNI es correcto");
+        } else {
+            System.out.println("Este DNI no es correcto");
+        }
+    }
 }
