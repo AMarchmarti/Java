7c68668b761d1c913baba6f8445919bae99849b3
AMarchmarti <marchmarti.antoni@gmail.com>
2019-01-16 18:22:39 +0100

    Created new class for test cases

--- POO/DNI/NewDni/src/dni/Dni.java
+++ POO/DNI/NewDni/src/dni/Dni.java
@@ -1,3 +1,5 @@
+package dni;
+
 public class Dni {
 
     private String dni = null;
--- POO/DNI/NewDni/src/dni/Tests.java
+++ POO/DNI/NewDni/src/dni/Tests.java
@@ -1,27 +1,7 @@
-public class AssignationTable {
+package dni;
 
-    char[] table = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V',
-            'H', 'L', 'C', 'K', 'E'};
+public class Tests {
 
-    public char getLetter (int posicion) {
-
-        return this.table[posicion];
-    }
-
-
-    public int getTableLength (){
-
-        return this.table.length;
-    }
-
-
-    public char findLetter (String Dni){
-
-        int posicion = Integer.parseInt(Dni) % getTableLength();
-        return getLetter(posicion);
-    }
-
-    /* Tests cases */
     public static void main(String[] args) {
 
         AssignationTable tabla = new AssignationTable();
