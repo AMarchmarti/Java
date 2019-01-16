9129ae24c1841d4d251a7897ac2f85152c159de8
AMarchmarti <marchmarti.antoni@gmail.com>
2019-01-16 17:59:02 +0100

    Fisrt test case correct

--- POO/DNI/NewDni/src/AssignationTable.java
+++ POO/DNI/NewDni/src/AssignationTable.java
@@ -1,4 +1,37 @@
 public class AssignationTable {
 
+    char[] table = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V',
+            'H', 'L', 'C', 'K', 'E'};
+
+    public char getLetter (int posicion) {
+
+        return this.table[posicion];
+    }
+
+
+    public int getTableLength (){
+
+        return this.table.length;
+    }
+
+
+    public char findLetter (String Dni){
+
+        int posicion = Integer.parseInt(Dni) % getTableLength();
+        return getLetter(posicion);
+    }
+
+    /* Tests cases */
+    public static void main(String[] args) {
+
+        AssignationTable tabla = new AssignationTable();
+
+        String Dni = "43180005";
+        char letter = tabla.findLetter(Dni);
+        char healthyletter = 'N';
+        if (letter == healthyletter){
+            System.out.println("Este DNI es correcto");
+        }
+    }
 
 }
