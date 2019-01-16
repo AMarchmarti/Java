f79888906d9ee4496f846dc499bd73805d93f233
AMarchmarti <marchmarti.antoni@gmail.com>
2019-01-16 17:26:20 +0100

    Start classes

--- POO/DNI/NewDni/src/Dni.java
+++ POO/DNI/NewDni/src/Dni.java
@@ -1,2 +1,9 @@
 public class Dni {
+
+    private String dni = null;
+    private boolean numeroSano = false;
+    private boolean letraSana = false;
+    private boolean dniCifSano = false;
+    private AssignationTable table = new AssignationTable();
+
 }
