043ab6d6745a96b23a4ce545e5e4c01e2f5405f5
AMarchmarti <marchmarti.antoni@gmail.com>
2019-01-16 19:04:45 +0100

    Created encapsulated functions and constructor

--- POO/DNI/NewDni/src/dni/Dni.java
+++ POO/DNI/NewDni/src/dni/Dni.java
@@ -3,9 +3,55 @@ package dni;
 public class Dni {
 
     private String dni = null;
-    private boolean numeroSano = false;
-    private boolean letraSana = false;
-    private boolean dniCifSano = false;
+    private Boolean numeroSano = false;
+    private Boolean letraSana = false;
+    private Boolean dniSano = false;
     private AssignationTable table = new AssignationTable();
+
+    public Dni (String dni){
+        this.dni = dni;
+    }
+
+
+    public void setDni(String cadena){
+        this.dni = cadena;
+    }
+
+
+    public String getDni(){
+        return this.dni;
+    }
+
+
+    private void setNumeroSano(Boolean valor){
+        this.numeroSano = valor;
+    }
+
+
+    public Boolean getNumeroSano (){
+        return this.numeroSano;
+    }
+
+
+    private void setLetraSana(Boolean valor){
+        this.letraSana = valor;
+    }
+
+
+    public Boolean getLetraSana() {
+        return this.letraSana;
+    }
+
+
+    public void setDniSano (Boolean valor){
+        this.dniSano = valor;
+    }
+
+
+    public Boolean getDniSano (){
+        return this.dniSano;
+    }
+
+    
 
 }
