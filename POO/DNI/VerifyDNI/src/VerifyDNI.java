ddee469f9506b68f7e76135a83688da98a1cfac7
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-01-16 12:33:43 +0100

    Start project verify DNI

---
+++ POO/DNI/VerifyDNI/src/VerifyDNI.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class VerifyDNI {
+
+    public void verifyDni(){
+
+        String readKeyboard = readKeyboard();
+        verify(readKeyboard);
+
+
+    }
+
+    public String readKeyboard(){
+
+        Scanner scanner = new Scanner(System.in);
+        String read = scanner.nextLine();
+        return read;
+    }
+
+
+    private boolean checkLength(String read){
+
+        if (read.length() == 10) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private boolean verify(String read){
+
+        String dic = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        checkLength(read);
+        try{
+            Integer.read.substring(0,8);
+            boolean dni = true;
+
+        }catch (Exception e){
+            return false;
+        }
+        if (dic.indexOf(read.substring(9)) != -1){
+
+            return false
+        }
+        else{
+            boolean letter = true;
+        }
+        if (dni && letter){
+            return true;
+        }
+    }
+
+
+
+
+}
