9b035057c40a83962db0685ffc2cc53e85741098
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-01-19 18:07:28 +0100

    Pendiente de revision

--- POO/DNI/NewDni/src/dni/MainDni.java
+++ POO/DNI/NewDni/src/dni/MainDni.java
@@ -1,32 +1,8 @@
 package dni;
 
-import java.io.IOException;
-import java.util.ArrayList;
-
-
 public class MainDni {
-    public static void main(String[] args){
-        TestCasesDni tests = new TestCasesDni();
-        ArrayList<String> casosTest = tests.getTest();
-        for(String dni : casosTest){
-            Dni objetoDniCif = new Dni(dni);
-            System.out.print(objetoDniCif.getDni());
+    public static void main(String[] args) throws Exception{
+        TestCasesDni dni = new TestCasesDni();
+        dni.testCases();}
 
-            if( objetoDniCif.checkDniCif()){
-                System.out.println(" PASS");
-            }
-            else
-                System.out.println(" FAIL");
-
-            System.out.println("dni --> " + objetoDniCif.getNumeroSano());
-            System.out.println("letra --> " + objetoDniCif.getLetraSana());
-            try{
-                System.out.println("La letra debería ser --> " + objetoDniCif.obtenerLetra());
-            }
-            catch(IOException ioexcepcion){
-                System.out.println("La letra es --> " + objetoDniCif.getLetters());
-            }
-        }
-
-    }
 }
--- POO/DNI/NewDni/src/dni/TestCasesDni.java
+++ POO/DNI/NewDni/src/dni/TestCasesDni.java
@@ -1,19 +1,17 @@
 package dni;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TestCasesDni {
 
-    private ArrayList<String> test = null;
 
-    private void setTest(ArrayList<String> test) {
-        this.test = test;
-    }
-
-    public ArrayList getTest() {
-        return this.test;
+    public void testCases() throws Exception {
+        List<String> casos = fileToList();
+        passTestCases(casos);
     }
 
     private ArrayList<String> fileToList() throws Exception {
@@ -24,8 +22,28 @@ public class TestCasesDni {
             String caso = test.nextLine();
             casosTest.add(caso);
         }
-        setTest(casosTest);
         return casosTest;
     }
-}
 
+    private void passTestCases(List<String> casosTest) {
+
+        for (String dni : casosTest) {
+            Dni objetoDniCif = new Dni(dni);
+            System.out.print(objetoDniCif.getDni());
+
+            if (objetoDniCif.checkDniCif()) {
+                System.out.println(" PASS");
+            } else
+                System.out.println(" FAIL");
+
+            System.out.println("dni --> " + objetoDniCif.getNumeroSano());
+            System.out.println("letra --> " + objetoDniCif.getLetraSana());
+            try {
+                System.out.println("La letra debería ser --> " + objetoDniCif.obtenerLetra());
+            } catch (IOException ioexcepcion) {
+                System.out.println("La letra es --> " + objetoDniCif.getLetters());
+            }
+        }
+
+    }
+}
