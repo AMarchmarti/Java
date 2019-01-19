9035bf61bfa3c35fdc9880c42b70956144729028
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-01-19 15:33:04 +0100

    Constructor, get functions and first step of program created

--- POO/BiciPalma/src/estacion/Estacion.java
+++ POO/BiciPalma/src/estacion/Estacion.java
@@ -1,2 +1,56 @@
+package estacion;
+
+import bicicleta.Bicicleta;
+
 public class Estacion {
+
+        private int id = 0;
+        private String direccion = null;
+        private int numeroAnclajes = 0;
+        private Bicicleta [] anclajes = null;
+
+
+        public Estacion (int id, String direccion, int numeroAnclajes){
+
+                this.id = id;
+                this.direccion = direccion;
+                this.numeroAnclajes = numeroAnclajes;
+                this.anclajes = new Bicicleta[anclajes];
+        }
+
+
+        public int getId(){
+                return this.id;
+        }
+
+
+        public String getDireccion(){
+                return this.direccion;
+        }
+
+
+        public int getNumeroAnclajes(){
+                return this.numeroAnclajes;
+        }
+
+
+        public void consultarEstacion(){
+                System.out.println("id: " + getId());
+                System.out.println("Direccion: " + getDireccion());
+                System.out.println("Numero de anclajes: " + getNumeroAnclajes());
+        }
+
+
+        public int anclajesLibres(){
+            int anclajesLibres = 0;
+            for (Bicicleta anclajes : this.anclajes){
+                    if (anclajes == null){
+                        anclajesLibres ++;
+                    }
+            }
+            return anclajesLibres;
+        }
+
+
+
 }
