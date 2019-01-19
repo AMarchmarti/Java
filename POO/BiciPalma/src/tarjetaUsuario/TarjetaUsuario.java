36928aa0e483319d2f0e55e9cba890d8f551152a
AMarchmarti <44237549+AMarchmarti@users.noreply.github.com>
2019-01-19 14:49:27 +0100

    Logic of TarjetaUsuario

--- POO/BiciPalma/src/tarjetaUsuario/TarjetaUsuario.java
+++ POO/BiciPalma/src/tarjetaUsuario/TarjetaUsuario.java
@@ -1,2 +1,21 @@
+package tarjetaUsuario;
+
 public class TarjetaUsuario {
+
+        private String id = null;
+        private Boolean activada = false;
+
+    public TarjetaUsuario(String id, Boolean valor) {
+        this.id = id;
+        this.activada = valor;
+
+    }
+
+    public void setActivada(Boolean valor){
+        this.activada = valor;
+    }
+
+    public Boolean getActivada(){
+        return this.activada;
+    }
 }
