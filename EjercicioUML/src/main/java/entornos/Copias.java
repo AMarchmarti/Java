42a10b2eb4e59916a7bc87e9b43ae994f3e67fc5
AMarchmarti <marchmarti.antoni@gmail.com>
2019-04-01 18:05:55 +0200

    Apicar prestamo

--- EjercicioUML/src/main/java/entornos/Copias.java
+++ EjercicioUML/src/main/java/entornos/Copias.java
@@ -1,4 +1,23 @@
 package entornos;
 
 public class Copias {
+
+    private Revista revista = null;
+    private Biblioteca bibliotecaPropietaria = null;
+    private String fecha = null;
+
+
+    /* ---- Constructor ---- */
+    public Copias() {
+
+    }
+
+    public Copias(Biblioteca biblioteca) {
+        this.bibliotecaPropietaria = biblioteca;
+    }
+
+    public Copias(Revista revista, Biblioteca bibliotecaPropietaria) {
+        this.revista = revista;
+        this.bibliotecaPropietaria = bibliotecaPropietaria;
+    }
 }
--- EjercicioUML/src/main/java/entornos/CopiasLibros.java
+++ EjercicioUML/src/main/java/entornos/CopiasLibros.java
@@ -2,5 +2,28 @@ package entornos;
 
 public class CopiasLibros extends Copias {
 
+    /* ---- Atributos ---- */
+    private Libros libro = null;
+    private String editorial = null;
+    private Miembros miembro = null;
+    private Personal empleado = null;
+
+
+    /* ---- Constructor ---- */
+    public CopiasLibros() {
+
+    }
+
+    public CopiasLibros(Libros libro) {
+        this.libro = libro;
+    }
+
+    public CopiasLibros(Biblioteca bibliotecaPropietaria, Libros libro) {
+        super(bibliotecaPropietaria);
+        this.libro = libro;
+    }
+
+    // Lógica
+
 
 }
